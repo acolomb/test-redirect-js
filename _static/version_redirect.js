dcdc2f55bc9e10f2a0a6a44d2950517df15c1a19
André Colomb <src@andre.colomb.de>
2022-01-14 16:48:07 +0100

    simple findBestVersion()

--- _static/version_redirect.js
+++ _static/version_redirect.js
@@ -7,8 +7,21 @@ console.log('Hello script!', versionParam);
 //const versionsAvailable = require('./versions.json');
 
 
-function findBestVersion(version) {
-    
+function findBestVersion(version, available) {
+    var bestVersion = '';
+    available.sort().reverse().some(function (element) {
+        if (version.startsWith(element)) {
+            if (bestVersion.length < element.length) {
+                bestVersion = element;
+            }
+        } else if (bestVersion) {
+            // Abort when we already had a prefix match, but advanced past it in
+            // the sorted list.
+            return true;
+        }
+        return false;
+    });
+    return bestVersion;
 }
 
 function redirectToVersion(version) {
